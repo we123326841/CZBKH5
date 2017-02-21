f2d03a0f79018d036113fee7819d7b4b26174263
we123326841 <wanghao@wanghaodeMacBook-Pro-3.local>
2017-02-21 10:53:25 +0800

    你他妈的爽了吗
    你他妈的爽了吗

--- ajax/check.php
+++ ajax/check.php
@@ -1,14 +1,19 @@
 <?php
 
-$username = $_GET['username'];
 
-$password = $_GET['password'];
+echo '<div>嘻嘻嘻</div>'
 
-if($username == "admin" && $password == "123"){
-    echo 2;
-}else{
-    echo 1;
-}
+//$username = $_GET['username'];
+
+//$password = $_GET['password'];
+
+
+
+//if($username == "admin" && $password == "123"){
+ //   echo '2username'.$username.'password'.$password;
+//}else{
+  //  echo '1username'.$username.'password'.$password;
+//}
 
 
 ?>
---
+++ ajax/form.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+<?php
+echo '<div id="show">你他妈的爽了吗</div>'
+?>
+<form>
+    用户名<input type="text" id="user"/><br>
+    密码<input type="text" id="pwd"/><br>
+    <input type="button" id="btn" value="请求"/>
+</form>
+
+<?php
+echo '<div id="show">我日你老猛犸</div>'
+?>
+</body>
